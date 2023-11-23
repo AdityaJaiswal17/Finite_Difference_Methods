d29a6bc0567139a8c217fa4d1815c198c7cdf5c1
Aditya Jaiswal <139119602+AdityaJaiswal17@users.noreply.github.com>
2023-11-23 05:54:04 +0530

    Update run_serial.sh

--- Lid_Driven_Cavity/run_serial.sh
+++ Lid_Driven_Cavity/run_serial.sh
@@ -5,7 +5,7 @@ g++ -O3 lid_driven_cavity_serial.cpp -lm;
 ./a.out;
 gnuplot -e "set pm3d map; set size ratio 1.0; sp 'output_serial.dat' u 1:2:7 w image; pause -1"
 mkdir serial_output
-mv output_serial.dat U-Re_x=0.5_serial.dat V-Re_x=0.5_serial.dat ~/Desktop/CFD/FVM_FDM/Lid_driven_cavity/serial_output
+mv output_serial.dat U-Re_x=0.5_serial.dat V-Re_x=0.5_serial.dat "(location upto to serial_output directory)"/serial_output
 
 
 
